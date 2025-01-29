142cbacd2060d125b0b2db7ed5db123505075788
francescawaters <francescawaters26@outlook.com>
2025-01-29 11:30:43 +0000

    Add: implement game logic for addition and initialize game on page load

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,13 @@ document.addEventListener("DOMContentLoaded", function () {
         alert("You clicked Submit!");
       } else {
         let gameType = this.getAttribute("data-type");
-        alert(`You clicked ${gameType}`);
+        runGame(gameType);
       }
     });
   }
+
+  runGame("addition");
+
 });
 
 /**
@@ -21,11 +24,18 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
 
     // Create random numbers between 1 and 25
     let num1 = Math.ceil(Math.random() * 25);
     let num2 = Math.ceil(Math.random() * 25);
+
+    if(gameType === "addition") {
+      displayAdditionQuestion(num1, num2);  
+    } else {
+      alert(`Unknown game type: ${gameType}`);
+      throw `Unknown gmae type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {}
@@ -36,7 +46,11 @@ function incrementScore() {}
 
 function incrementWrongAnswer() {}
 
-function displayAdditionQuestion() {}
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+}
 
 function displaySubtractQuestion() {}
 
